f1b2ab83e38b3841bfd0e6348f77578b9a25b3b6
Nikolas Santana de Arruda <nikolasdssantana@gmail.com>
2026-03-01 10:08:14 -0300

    refactor: update main entry with providers

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { queryClient } from '@/lib/queryClient'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
