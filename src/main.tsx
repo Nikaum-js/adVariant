29252b1a9d86df6d97ecd95c470f13dd8bdea9d8
Nikolas Santana de Arruda <nikolasdssantana@gmail.com>
2026-03-28 14:28:18 -0300

    ✨ feat: add TooltipProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { queryClient } from '@/lib/queryClient'
 import './index.css'
 import App from './App.tsx'
@@ -8,7 +9,9 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </QueryClientProvider>
   </StrictMode>
 )
