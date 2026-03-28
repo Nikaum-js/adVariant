1d633be2a3207c75559d4e98d91ff59d3a43d230
Nikolas Santana de Arruda <nikolasdssantana@gmail.com>
2026-03-28 09:47:58 -0300

    ✨ feat: add success, warning, info badge variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -14,6 +14,12 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
         outline: 'text-foreground',
+        success: 'border-transparent bg-success text-success-foreground shadow hover:bg-success/80',
+        'success-muted': 'border-transparent bg-success-muted text-success',
+        warning: 'border-transparent bg-warning text-warning-foreground shadow hover:bg-warning/80',
+        'warning-muted': 'border-transparent bg-warning-muted text-warning',
+        info: 'border-transparent bg-info text-info-foreground shadow hover:bg-info/80',
+        'info-muted': 'border-transparent bg-info-muted text-info',
       },
     },
     defaultVariants: {
